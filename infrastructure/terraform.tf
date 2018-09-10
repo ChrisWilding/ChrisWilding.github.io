5b6168a3db52616ef583c29fc3e04d5e275ff3f4
Chris Wilding <hello@chriswilding.co.uk>
2018-09-10 18:29:12 +0100

    Update Let's Encrypt certificate

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -284,6 +284,7 @@ resource "aws_route53_record" "website_route53_letsencrypt" {
   records = [
     "xWz6bWsL0WPQTManU1yuivos_-iMccoZcE6nW71YgyI",
     "CbP-_Fe2a-4_yxYfAOyC82xkywrE0azkpE83qf_H0X0",
+    "zyTPstw12S17JnD-Ycy4dWO-42FQ2oRSf_unClu7SGw",
   ]
 
   ttl     = "300"
