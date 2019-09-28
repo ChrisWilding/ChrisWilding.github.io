65603cb1f7a71783d7647c8b8396efce79d4dcdc
Chris Wilding <hello@chriswilding.co.uk>
2019-09-28 12:54:46 +0100

    Update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Chris Wilding`,
+    description: `Chris Wilding is a software engineer living and working in Manchester, UK`,
+    author: `@ChrisWildingUK`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
