dfea09d7daf04dac6054689d1447549b8852d8a7
Chris Wilding <hello@chriswilding.co.uk>
2019-09-29 19:32:19 +0000

    Update content (#13)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,67 @@
 import React, { FunctionComponent } from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage: FunctionComponent = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage: FunctionComponent = () => {
+  const {
+    site: {
+      siteMetadata: { author, email, github, linkedin },
+    },
+  } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            author
+            email
+            github
+            linkedin
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi, I&#39;m Chris Wilding</h1>
+      <p>I&#39;m a software engineer living and working in Manchester, UK</p>
+      <p>
+        If you want to find our more about me take a look at my{" "}
+        <Link to="/about/">about</Link> page or{" "}
+        <a
+          href={`https://github.com/${github}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          GitHub
+        </a>{" "}
+        profile. If you want to get in touch contact me through{" "}
+        <a
+          href={`https://uk.linkedin.com/in/${linkedin}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          LinkedIn
+        </a>{" "}
+        or by <a href={`mailto:${email}`}>email</a>.
+      </p>
+      <p>
+        I occasionally tweet as{" "}
+        <a
+          href={`https://twitter.com/${author}`}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          {author}
+        </a>
+        .
+      </p>
+    </Layout>
+  )
+}
 
 export default IndexPage
